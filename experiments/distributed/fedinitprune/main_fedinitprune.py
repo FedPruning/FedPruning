143dc1cb2fc0130e189ffc8735ff0a66c4dd751f
px20070435 <zhihao.yu.th@dartmouth.edu>
2024-10-22 02:05:14 +0800

    Update main_fedinitprune.py

--- experiments/distributed/fedinitprune/main_fedinitprune.py
+++ experiments/distributed/fedinitprune/main_fedinitprune.py
@@ -202,7 +202,7 @@ if __name__ == "__main__":
     # initialize the wandb machine learning experimental tracking platform (https://www.wandb.com/).
     if process_id == 0:
         wandb.init(
-            project="fedprune",
+            project="FedPruning",
             name="FedInitprune_"
             + args.dataset 
             + "_"
